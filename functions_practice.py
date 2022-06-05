447712c69d8a0fe4ae7175be4aadd4a3652524ab
Ayda Mkaddem <92482497+AydaMkd@users.noreply.github.com>
2022-06-05 11:06:39 -0500

    first commit

---
+++ functions_practice.py
@@ -0,0 +1,22 @@
+from ntpath import join
+
+
+def hello():
+    print("greetings!")
+hello()
+
+def pack(a,b,c):
+    return[a,b,c]
+x=pack("cat","kitty","kittyCat")
+print(x)
+
+def eat_lunch(list):
+    if len(list)==0:
+        print("My lunchbox is empty!")
+    else:
+        print("I first eat"+list[0])
+        list1=list[1:(len(list)+1)]
+    for i in range(len(list1)):
+        print("Next I eat "+ list1[i])
+       
+eat_lunch(["oats", "salad", "soup"])
