523871bf91e28d7abb247c62192bb43d64b227d0
Ayda Mkaddem <92482497+AydaMkd@users.noreply.github.com>
2022-06-05 11:16:58 -0500

    commit

--- functions_practice.py
+++ functions_practice.py
@@ -7,14 +7,15 @@ hello()
 
 def pack(a,b,c):
     return[a,b,c]
-x=pack("cat","kitty","kittyCat")
-print(x)
+# x=pack("cat","kitty","kittyCat")
+pack("kitty","cat","kittyCat")
+print(pack("kitty","cat","kittyCat"))
 
 def eat_lunch(list):
     if len(list)==0:
         print("My lunchbox is empty!")
     else:
-        print("I first eat"+list[0])
+        print("I first eat "+list[0])
         list1=list[1:(len(list)+1)]
     for i in range(len(list1)):
         print("Next I eat "+ list1[i])
